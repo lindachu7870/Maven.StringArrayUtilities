9d5ca048cfb7ab31af65563219f26c5b6ff0505d
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-02 12:05:08 -0400

    finished getnumberofoccurences

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -12,22 +12,26 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
-        if (array.length > 0) {
-            return array[0];
-        }
-        return null;
+        return array[0];
     }
+//        if (array.length > 0) {
+//            return array[0];
+//        }
+//        return null;
+//    }
 
     /**
      * @param array array of String objects
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
-        if (array.length > 0) {
-            return array[1];
-        }
-        return null;
+        return array[1];
     }
+//        if (array.length > 0) {
+//            return array[1];
+//        }
+//        return null;
+//    }
 
     /**
      * @param array array of String objects
@@ -95,7 +99,10 @@ public class StringArrayUtils {
         for (String s : array) {
             String lowerCaseStr = s.toLowerCase();
             for (int i = 0; i < lowerCaseStr.length(); i++) {
-                set.add(lowerCaseStr.charAt(i));
+                // Get current letter
+                char currentLetter = lowerCaseStr.charAt(i);
+                // Make sure current letter is only alphabet
+                if (currentLetter >= 97 && currentLetter <= 122) set.add(currentLetter);
                 if (set.size() == 26) return true;
             }
         }
@@ -108,7 +115,13 @@ public class StringArrayUtils {
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
